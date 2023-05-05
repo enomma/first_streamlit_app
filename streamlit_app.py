4b541e809b8d08e744800911d73e19e03c01c4d4
enomma <132085570+enomma@users.noreply.github.com>
2023-05-05 10:25:07 -0600

    Update streamlit_app.py
    added another text entry box for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#Add another text entry box to display fruityvice api response
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+streamlit.write('Thanks for adding', fruit_choice)
+
